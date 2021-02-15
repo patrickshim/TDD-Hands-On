4cbdec63d0882d14a590a4de6a6d8b8cc07fc746
shimp817 <shimp@oregonstate.edu>
2021-02-15 17:06:13 -0500

    modified check_pwd to make sure password length less than 1 is not accepted

--- check_pwd.py
+++ check_pwd.py
@@ -2,7 +2,7 @@
 #https://stackoverflow.com/questions/41117733/validation-of-a-password-python
 
 def check_pwd(pwd):
-  if len(pwd) == 1:
+  if len(pwd) <= 1:
     return False
-    
+
   return True
